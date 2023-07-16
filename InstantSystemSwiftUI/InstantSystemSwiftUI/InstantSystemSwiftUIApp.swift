50e8c73ac76c95420a590dc9c7d79e6468e4c2e9
Gdonzeau <gdonzeau@hotmail.com>
2023-07-16 19:21:51 +0200

    Création d'un projet en SwiftUI

---
+++ InstantSystemSwiftUI/InstantSystemSwiftUI/InstantSystemSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantSystemSwiftUIApp.swift
+//  InstantSystemSwiftUI
+//
+//  Created by Guillaume on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstantSystemSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
